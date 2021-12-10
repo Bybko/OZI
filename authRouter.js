6fd075aa1903dbcac57789d042e4457fd6e12cfc
dima203 <gg37500@yandex.by>
2021-12-10 19:19:04 +0300

    add req

--- authRouter.js
+++ authRouter.js
@@ -4,13 +4,17 @@ const controller = require('./authController')
 const {check} = require("express-validator") //Экспортируем валидатор-функцию check из данного модуля (нужна для проверки). Эта функция передаётся вторым параметром,
 //их можно передать несколько, передаются как массив
 
+const bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 //Запрос и вызов соответстующей запросу функции:
-router.post('/registration', [                                  //Запрос на регистрацию
+router.post('/registration', urlencodedParser, [                                  //Запрос на регистрацию
 	//Валидируем на кол-во символов (чтобы логи и пароль не были пустыми, а также вводит ограничение на длину пароля)
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
     check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
 ], controller.registration)
-router.post('/login', controller.login) //Запрос на логин
+router.post('/login', urlencodedParser, controller.login) //Запрос на логин
 router.get('/users', controller.getUsers) //Запрос на различные доступы (разные уровни доступа)\
 
 module.exports = router //Экспортируем этот объект в другие модули
